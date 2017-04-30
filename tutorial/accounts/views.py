92b634c6b4c9c17275273683967754e22c191616
Rahul Kumar <kmrrahul7@gmail.com>
2017-04-30 18:15:41 +0530

    changed few codes

--- tutorial/accounts/views.py
+++ tutorial/accounts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render,HttpResponse
-
+from django.shortcuts import render,HttpResponse, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .form import RegistrationForm
 # Create your views here.
 #two type of views 1.function based views and 2nd is classes based views
 
@@ -8,3 +9,17 @@ def home(request):
     name='Rahul Kumar'
     args={'myname':name,'numbers':number}
     return render(request,'accounts/home.html',args)
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        print("save")
+        if form.is_valid():
+            print("not save")
+            form.save()
+            return redirect('/account')
+        
+    else:
+        form=RegistrationForm()
+        args={'form':form}
+        return render(request,'accounts/reg_form.html',args)
